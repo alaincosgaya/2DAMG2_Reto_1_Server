e0c9f35f923a35776f1a9ceeef8544101b215b6a
2dam <2dam@HZ375750.TartangaLH.eus>
2021-10-26 09:46:00 +0200

    Pool de conexiones

---
+++ src/socket/Server.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package socket;
+
+import java.net.ServerSocket;
+
+/**
+ *
+ * @author 2dam
+ */
+public class Server {
+     private final static int port = 5001;
+     
+     ServerSocket server = null;
+}
